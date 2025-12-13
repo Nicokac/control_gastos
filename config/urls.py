3b0a728a65f654ad1aed593f44e5b128a00a51d8
Nicolás Kachuk <kachuk_nm@hotmail.com>
2025-12-13 19:24:28 -0300

    feat: configurar ruta principal URL

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,25 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include( 'apps.users.urls')),
+    path("categories/", include('apps.categories.urls')),
+    path("expenses/", include('apps.expenses.urls')),
+    path("income/", include('apps.income.urls')),
+    path("savings/", include('apps.savings.urls')),
+    path("budgets/", include('apps.include.urls')),
+    path("dashboard/", include('apps.reports.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+
+    # Debug Toolbar
+    import debug_toolbar
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
