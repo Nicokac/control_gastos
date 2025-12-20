3101103a542ffb71ef4a057f68422cbdc6520cb6
Nicolás Kachuk <kachuk_nm@hotmail.com>
2025-12-20 17:33:56 -0300

    feat(expenses): configurar URLs.

--- apps/expenses/urls.py
+++ apps/expenses/urls.py
@@ -1,4 +1,6 @@
-"""URLs para la app de gastos."""
+"""
+URLs para la app de gastos.
+"""
 
 
 from django.urls import path
@@ -9,4 +11,7 @@ app_name = 'expenses'
 urlpatterns = [
     path('', views.ExpenseListView.as_view(), name='list'),
     path('create/', views.ExpenseCreateView.as_view(), name='create'),
+    path('<int:pk>/', views.ExpenseDetailView.as_view(), name='detail'),
+    path('<int:pk>/edit/', views.ExpenseUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete/', views.ExpenseDeleteView.as_view(), name='delete'),
 ]
