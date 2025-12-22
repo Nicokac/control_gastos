91682f018e176d2f6ed5be974feb2f95dce777ff
Nicolás Kachuk <kachuk_nm@hotmail.com>
2025-12-21 22:46:07 -0300

    feat(savings): configurar URLs.

--- apps/savings/urls.py
+++ apps/savings/urls.py
@@ -1,3 +1,7 @@
+"""
+URLs para la app de ahorro.
+"""
+
 from django.urls import path
 from . import views
 
@@ -6,4 +10,8 @@ app_name = 'savings'
 urlpatterns = [
     path('', views.SavingListView.as_view(), name='list'),
     path('create/', views.SavingCreateView.as_view(), name='create'),
+    path('<int:pk>/', views.SavingDetailView.as_view(), name='detail'),
+    path('<int:pk>/edit/', views.SavingUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete/', views.SavingDeleteView.as_view(), name='delete'),
+    path('<int:pk>/movement/', views.SavingMovementCreateView.as_view(), name='add_movement'),
 ]
